ae6890f4c0cbf12892e79e0c3e78e1ab36818046
cs <cs.temporary@icloud.com>
2018-09-07 04:14:54 +0000

    Set version to 1.0.0 in docs

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, U8N WXD <https://github.com/U8NWXD/>'
 author = 'U8N WXD <https://github.com/U8NWXD/>'
 
 # The short X.Y version
-version = ''
+version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
