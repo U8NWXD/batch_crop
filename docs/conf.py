00d046006986ee636d7eec2b9e3218692545c917
cs <cs.temporary@icloud.com>
2018-09-09 01:11:31 +0000

    Make conf.py packages path more robust

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+path = os.path.join(os.path.abspath(__file__), "..", "..")
+sys.path.insert(0, os.path.abspath(path))
 
 
 # -- Project information -----------------------------------------------------
