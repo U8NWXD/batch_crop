0ac7d7e173bc55af52661de2344955b29fcf5917
cs <cs.temporary@icloud.com>
2018-09-09 00:45:16 +0000

    Add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='batch_crop',
+    version='1.0.0',
+    packages=['batch_crop'],
+    url='https://www.github.com/U8NWXD/batch_crop',
+    license='AGPLv3',
+    author='U8N WXD',
+    author_email='cs.temporary@icloud.com',
+    description='A Python utility for batch cropping images',
+    install_requires=['rawpy', 'Pillow']
+)
