122a121da5e64b01b948959308b29113354e7650
cs <cs.temporary@icloud.com>
2018-09-09 04:40:24 +0000

    Add batch_crop import to conf.py to fix autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,8 @@ import sys
 path = os.path.join(os.path.abspath(__file__), "..", "..")
 sys.path.insert(0, os.path.abspath(path))
 
+import batch_crop
+
 
 # -- Project information -----------------------------------------------------
 
